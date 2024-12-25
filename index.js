5d9128f75a5d233faad7e5642d7ab76eb7a09b93
Developer-Madhu <madhuatomix@gmail.com>
2024-12-25 18:22:02 +0530

    changed api key

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-document.addEventListener('DOMContentLoaded', ()=>{
+APIKEY = '111b652a504586cd0c97d6bc0f93a1ad';
+
+document.addEventListener('DOMContentLoaded', () => {
     var inputVal = document.getElementById('task-input');
     var btn = document.getElementById('addbtn');
     var city = document.getElementById('cityname')
@@ -7,28 +9,26 @@ document.addEventListener('DOMContentLoaded', ()=>{
     var msg = document.getElementById('ermsg')
     var winds = document.getElementById('wind')
 
-    const APIKEY = '111b652a504586cd0c97d6bc0f93a1ad';
-
-    btn.addEventListener('click', async ()=>{
+    btn.addEventListener('click', async () => {
         var cityName = inputVal.value.trim();
-        if(!cityName) displayError();
+        if (!cityName) displayError();
         const data = await fetchWeatherData(cityName);
         displayWeatherData(data);
     })
 
-    async function fetchWeatherData(cityName){
+    async function fetchWeatherData(cityName) {
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${APIKEY}`
         const response = await fetch(url)
-        if(!response.ok){
+        if (!response.ok) {
             displayError()
-        }else{
+        } else {
             const data = await response.json()
             return data
         }
 
     }
 
-    function displayWeatherData(wdata){
+    function displayWeatherData(wdata) {
         var weather0 = wdata.weather[0].main
         var descs = "Info : " + wdata.weather[0].description
         var temps = "Temperature : " + wdata.main.temp
@@ -37,7 +37,6 @@ document.addEventListener('DOMContentLoaded', ()=>{
         desc.innerHTML = descs
         temp.innerHTML = temps
         winds.innerHTML = winddata
-        console.log(wdata)
     }
 
     const displayError = () => {
